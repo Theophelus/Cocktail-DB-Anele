a93f8ad43e31b7f11d1124bd3d3278d910fca595
Theophelus <aneletom10@gmail.com>
2024-03-09 13:56:49 +0200

    feature data model to read baseUrl and BasePath with data.json file this can be expanded and the project grows

---
+++ src/main/java/model/FeatureData.java
@@ -0,0 +1,19 @@
+package model;
+
+/*
+ feature data model to read baseUrl and BasePath for now from data.json file
+ this can be expanded and the project grows
+ */
+
+public class FeatureData {
+    private String cocktailDbEndpoint;
+    private String cocktailDbBasePath;
+
+    public String getCocktailDbEndpoint() {
+        return cocktailDbEndpoint;
+    }
+    public String getCocktailDbBasePath() {
+        return cocktailDbBasePath;
+    }
+
+}
