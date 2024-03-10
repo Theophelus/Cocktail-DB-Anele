000ee406ea9033af36f554b404d69bee99ab1080
Theophelus <aneletom10@gmail.com>
2024-03-10 09:54:52 +0200

    adding a deserializeJsonResponse method to deserialize json response to ingredients object

--- src/main/java/utils/DataFileReaderUtils.java
+++ src/main/java/utils/DataFileReaderUtils.java
@@ -1,24 +1,43 @@
 package utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import model.FeatureData;
+import io.restassured.response.Response;
+import model.FeatureDataModel;
+import model.Ingredients;
 
 import java.io.File;
+import java.io.IOException;
 
 public class DataFileReaderUtils {
 
 
-    public static FeatureData getDataFile(String filePath){
-
+    public static FeatureDataModel getDataFile(String filePath){
         ObjectMapper mapper = new ObjectMapper();
 
         try{
 
             File file = new File("src/test/resources/data/"+filePath);
-            return mapper.readValue(file, FeatureData.class);
+            return mapper.readValue(file, FeatureDataModel.class);
 
+        }catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /*
+        - deserializeJsonResponse call take a jsonResponse params
+        - define a jackson Object mapper object, to read json data
+        - deserialize json body to Ingredients class
+     */
+    public static Ingredients deserializeJsonResponse(Response jsonResponse){
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(jsonResponse.getBody().asString(), Ingredients.class);
         }catch (Exception e){
             e.printStackTrace();
+
             return null;
         }
     }
