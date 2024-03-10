14a1f79dc83f0f60fdd7baeb5ad2583170260d0a
Theophelus <aneletom10@gmail.com>
2024-03-10 16:58:48 +0200

    adding a method to search by ingredient with alcoholic, then Alcohol is Yes and ABV is null

--- src/test/java/tests/SearchIngredientsByNameTest.java
+++ src/test/java/tests/SearchIngredientsByNameTest.java
@@ -15,7 +15,7 @@ public class SearchIngredientsByNameTest {
     @Test(description = "Verify Cocktail DB is able to search an ingredient by provided name and return specific fields.")
     public void userShouldBeAbleToSearchIngredientByNameAndValidateResponse() throws IOException {
         String ingredientsName = "Vodka";
-        String descriptions ="Vodka is a distilled beverage " +
+        String descriptions = "Vodka is a distilled beverage " +
                 "composed primarily of water and ethanol";
         //get searchIngredientByName method
         Ingredient ingredients = SearchIngredients.searchIngredientByName(ingredientsName);
@@ -32,7 +32,7 @@ public class SearchIngredientsByNameTest {
         Assert.assertEquals(ingredients.getId(), "1");
         Assert.assertEquals(ingredients.getName(), ingredientsName);
         assertTrue(ingredients.getDescription().contains(descriptions),
-                "Provided description does not contain provided text: '"+descriptions+"'  ");
+                "Provided description does not contain provided text: '" + descriptions + "'  ");
         Assert.assertEquals(ingredients.getType(), ingredientsName);
         Assert.assertEquals(ingredients.getAlcoholContent(), "Yes");
         Assert.assertEquals(ingredients.getAbv(), "40");
@@ -54,13 +54,40 @@ public class SearchIngredientsByNameTest {
         if (ingredient.getName().equalsIgnoreCase("No")) {
 
             assertTrue(ingredient.getAlcoholContent().equalsIgnoreCase("No"));
-            assertTrue(ingredient.getAbv().equalsIgnoreCase("null"),
-                    "ABV has value: '" + ingredient.getAbv() + "' ");
+            assertNull(ingredient.getAbv(),
+                    "ABV should be null for an non-alcoholic ingredient:" +
+                            " '" + ingredient.getAbv() + "' ");
 
-        } else{
+        } else {
             assertTrue(ingredient.getName().equalsIgnoreCase(nonAlcoholicIngredient),
-                    "Provided ingredient is alcoholic");
+                    "Alcohol content should be No");
+        }
     }
+    @Test(description = "Verify a user can search by ingredient with alcoholic," +
+            "then Alcohol is Yes and ABV is null")
+    public void verifyIfIngredientIsAlcoholicVerifyResponse() {
+
+        //define non-alcoholic ingredient variable
+        String alcoholicIngredient = "Vodka";
+        //get searchByName method from
+        Ingredient ingredient = SearchIngredients.searchIngredientByName(alcoholicIngredient);
+        /*
+            check provided ingredient contains yes
+            if true, assert, alcohol = no and ABV = null
+         */
+        assert ingredient != null;
+        if (ingredient.getName().equalsIgnoreCase("Yes")) {
+
+            assertTrue(ingredient.getAlcoholContent().equalsIgnoreCase("Yes"),
+                    "Alcohol content should be Yes");
+            assertNotNull(ingredient.getAbv(),
+                    "ABV should not be null for an alcoholic ingredient: '" + ingredient.getAbv() + "' ");
+
+        } else {
+            assertTrue(ingredient.getAlcoholContent().equalsIgnoreCase("Yes"),
+                    "Alcohol content should not be Yes");
+        }
     }
+
 
 }
