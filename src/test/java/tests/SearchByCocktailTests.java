aeab4adb494406e4f20c366f079537be47782439
Theophelus <aneletom10@gmail.com>
2024-03-11 20:33:57 +0200

    Cleaning up SearchCocktailByTests class and adding more test cases

--- src/test/java/tests/SearchByCocktailTests.java
+++ src/test/java/tests/SearchByCocktailTests.java
@@ -1,8 +1,9 @@
 package tests;
-
-import apiCalls.SearchingByCocktail;
+import apiCalls.SearchingCocktailByName;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
+import model.Drink;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.*;
@@ -19,7 +20,7 @@ public class SearchByCocktailTests {
         Map<String, String> margarita = new HashMap<>();
         margarita.put("s", "Margarita");
         String cocktailJsonSchema = "cocktailJsonSchema.json";
-        Response httpResponse = SearchingByCocktail.getCocktailResponse(margarita);
+        Response httpResponse = SearchingCocktailByName.getCocktailResponse(margarita);
         // Validate response against JSON schema
         httpResponse
                 .then()
@@ -27,6 +28,34 @@ public class SearchByCocktailTests {
                 .statusCode(200)
                 .body(JsonSchemaValidator
                 .matchesJsonSchema(readJsonSchema(cocktailJsonSchema)));
+
+    }
+
+    @Test(description = "Verify user can search cocktails by name using Cocktail DB API")
+    public void searchCocktailsByName(){
+        //cocktail to search by name
+        String margarita = "Margarita";
+        //get search Cocktail by name method
+        Drink drink = SearchingCocktailByName.searchCockTailByName(margarita);
+        assert drink != null;
+        System.out.println(drink.getStrDrink());
+        //validate json response
+        Assert.assertEquals(drink.getStrDrink(), margarita);
+        Assert.assertNull(drink.getStrDrinkAlternate(),
+                "No alternative drinks for: '"+margarita+"'");
+        Assert.assertEquals(drink.getStrTags(), "IBA,ContemporaryClassic");
+        Assert.assertEquals(drink.getStrCategory(), "Ordinary Drink");
+        Assert.assertEquals(drink.getStrIBA(), "Contemporary Classics");
+        Assert.assertEquals(drink.getStrAlcoholic(), "Alcoholic");
+    }
+
+    @Test(description = "Verify which category is a particular cocktail belong to from Cocktail DB API")
+    public void cocktailBelongToWhichCategory(){
+        String cocktail = "Margarita";
+        Drink drink = SearchingCocktailByName.searchCockTailByName(cocktail);
+
+        assert drink != null;
+        Assert.assertTrue(drink.getStrCategory().equalsIgnoreCase("Ordinary Drink"));
 
     }
 
